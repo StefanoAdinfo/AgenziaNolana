4ac093b3168142ff8df9ac62ed3109c25d797188
StefanoAdinfo <s.daniello@adinfo.it>
2025-05-15 10:39:10 +0200

    fix

--- functions.php
+++ functions.php
@@ -551,3 +551,28 @@ function mostra_acf($atts)
 	return '';
 }
 add_shortcode('acf_field', 'mostra_acf');
+
+
+
+
+
+
+
+
+
+
+add_action('init', 'themeslug_register_block_styles');
+
+function themeslug_register_block_styles()
+{
+	register_block_style('core/image', array(
+		'name'         => 'hand-drawn',
+		'label'        => __('Hand Drawn', 'themeslug'),
+		'inline_style' => '.wp-block-image.is-style-hand-drawn img {
+			border: 2px solid currentColor;
+			overflow: hidden;
+			box-shadow: 0 4px  10px 0 rgba( 0, 0, 0, 0.3 );
+			border-radius: 255px 15px 225px 15px/15px 225px 15px 255px !important;
+		}'
+	));
+}
